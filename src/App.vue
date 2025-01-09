37e378c48c43a90dca7e9fa6fae7e8f5a8c4938e
chenhao <943525359@qq.com>
2025-01-09 19:24:57 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ElConfigProvider } from 'element-plus';
+import { RouterView } from 'vue-router'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+</script>
+
+<template>
+  <el-config-provider :locale="zhCn">
+    <RouterView />
+  </el-config-provider>
+</template>
+
+<style scoped>
+</style>
---
+++ src/layouts/index.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <div>
+        <!-- 左侧模式 -->
+    </div>
+</template>
